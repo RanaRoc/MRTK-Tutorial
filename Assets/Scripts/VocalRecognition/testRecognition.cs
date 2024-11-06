320044cb3b3281fd7b32ef4a5f6eb45dc9cdea1e
Romain <romamour04@gmail.com>
2024-11-06 17:34:53 +0100

    Test 'activated' ok

---
+++ Assets/Scripts/VocalRecognition/testRecognition.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+
+public class testVocalReconizer : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    public GameObject targetObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create keywords for keyword recognizer
+        keywords.Add("activate", () =>
+        {
+            // action to be performed when this keyword is spoken
+            Renderer renderer = targetObject.GetComponent<Renderer>();
+            renderer.material.color = Color.red;
+        });
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
