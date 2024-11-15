f6ddd49134dba66c032610b3accf006cacfae557
Romain <romamour04@gmail.com>
2024-11-15 16:26:07 +0100

    Adding next step + merge with view

--- Assets/Scripts/VocalRecognition/testRecognition.cs
+++ Assets/Scripts/VocalRecognition/testRecognition.cs
@@ -10,18 +10,26 @@ public class testVocalReconizer : MonoBehaviour
     KeywordRecognizer keywordRecognizer;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
     public GameObject targetObject;
+    UIScript UI;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        UI = GameObject.Find("InterfaceUtilisateur").GetComponent<UIScript>();
+
         //Create keywords for keyword recognizer
         keywords.Add("next", () =>
         {
-            // action to be performed when this keyword is spoken
-            Renderer renderer = targetObject.GetComponent<Renderer>();
-            renderer.material.color = Color.red;
+            UI.ChangeProgress();
         });
+        
+        //Create keywords for keyword recognizer
+        keywords.Add("end", () =>
+        {
+            UI.StopRecipe();
+        });
+
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
@@ -40,6 +48,28 @@ public class testVocalReconizer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Vérifier si la touche "E" est pressée
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key was pressed.");
+            OnEPressed();
+        }
+
+        // Vérifier si la touche "R" est pressée
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("R key was pressed.");
+            OnRPressed();
+        }
+    }
+
+    void OnEPressed()
+    {
+        UI.ChangeProgress();
+    }
+
+    void OnRPressed()
+    {
+        UI.StopRecipe();
     }
 }
